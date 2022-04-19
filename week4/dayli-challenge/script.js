39c6749eb8dd260c21728f0ecc3f2694a95fde2d
THISmann <fuhetienne0@gmail.com>
2022-04-19 21:56:39 +0100

    day1-end

--- week4/dayli-challenge/script.js
+++ week4/dayli-challenge/script.js
@@ -34,3 +34,56 @@ for (let index = 0; index < array.length; index++) {
     console.log("*");
 }
 
+// temperature exercise
+
+let temperature = prompt();
+
+if(temperature < 0 ){
+    console.log("etat solide");
+} else if(temperature >= 0 && temperature < 100){
+    console.log("etat liquide");
+} else if(temperature >= 100) {
+    console.log("etat gazeuz");
+}
+
+// Make a keyless car!
+
+driver = prompt();
+if(driver < 18){
+    console.log("sorry , you are to young to drive this car powering off");
+} else if (driver = 18){
+    console.log("congratulations on your first year of driving, Enjoy the ride !");
+} else if(driver > 18){
+    console.log("powering On. Enjoy the ride!");
+}
+
+    //exercise 1
+    
+    // question 1 
+    let data = {
+        username: "",
+        password: ""
+    }   
+
+    // question 2
+    let database = [data] 
+
+    // question 3 
+
+    let obj = {
+        username: "",
+        timeline: 0 
+    } 
+
+    let obj2 = {
+        username: "",
+        timeline: 0 
+    } 
+
+    let obj3 = {
+        username: "",
+        timeline: 0 
+    } 
+
+    let newfeed = [data, obj1, obj2 , obj3 ]
+
