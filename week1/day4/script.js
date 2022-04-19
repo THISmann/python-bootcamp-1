ec6c8dcd8ae15d7686eb94d8a06923b7b91582bb
THISmann <fuhetienne0@gmail.com>
2022-04-19 16:58:55 +0100

    start

---
+++ week1/day4/script.js
@@ -0,0 +1,139 @@
+alert('hello world!');
+
+// part 1 
+let addressNumber = 123;
+let addressStreet = "yaounde";
+let country = "cameroun";
+
+// part 2
+let globalAddress = "";
+globalAddress.concat("i live in ", addressStreet, addressNumber, "in ",country);
+
+// part 3
+console.log(globalAddress)
+
+// exercise 2 
+
+// question 1
+let birthDay = 1999;
+
+// question 2 
+let futureYears = 2023;
+
+// question 3
+let possibleAge = futureYears - birthDay ;
+
+// question 1 
+console.log("i will be NN in YYYY " + possibleAge);
+
+
+// exercise 3 
+
+// question 1 
+let pet =  ["cat", "dog", "fish", "rabbit", "cow"];
+
+// question 2
+console.log(pet[1]);
+
+// question 3
+pet.push("horse");
+
+
+// question 4
+let index = pet.indexOf("rabbit");
+pet.slice(index, 1);
+
+// question 5 
+pet.length
+
+
+
+// Exercise 1: Your Favorite Food
+
+// question 1
+let food = "okok";
+
+// question 2
+let meal = "lunch";
+
+// question 3
+console.log('I eat ' + food + ' at every ' + meal);
+
+
+
+// Exercise 3:
+
+// part 1
+
+// question1 
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length;
+
+// question2
+myWatchedSeriesSentence = myWatchedSeries.toString(myWatchedSeries);
+
+// question 3 
+console.log("i watched 3 series :" + myWatchedSeriesSentence);
+
+// part 2
+
+// question 1 
+myWatchedSeries.splice(indexOf("the big bang theory"), 1 , "Hint");
+
+// question 2 
+myWatchedSeries.push("scorpion");
+
+// question 3 
+myWatchedSeries.unshift("game of trone");
+
+// question 4 
+myWatchedSeries.splice(indexOf("black mirror") , 1);
+
+// question 5
+let id = myWatchedSeries.indexOf("money heist");
+console.log(myWatchedSeries[id][2])
+
+//question 6
+console.log(myWatchedSeries);
+
+
+// exercise 3 
+let cel_temp = 100;
+
+let hint = cel_temp/5*9 + 32;
+
+console.log("temperature in C" +cel_temp+ "is " + hint + "in F");
+
+
+// exercise 4 
+
+console.log(2+3)
+// Prediction: It will output 5, because 2 and 3 are numbers
+// Actual: 5
+
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction:
+// Actual:
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction:
+// Actual:
+
+// question 1 
+55
+
+// question 2
+23
+
+//question 3
+null    
+
+// question 4
+ 
+
