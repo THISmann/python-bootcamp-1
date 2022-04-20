51f7d38caa4490375ad14847905e67f348202456
THISmann <fuhetienne0@gmail.com>
2022-04-20 09:27:13 +0100

    exercises XP gold

--- week4/dayli-challenge/script.js
+++ week4/dayli-challenge/script.js
@@ -143,3 +143,88 @@ if(driver < 18){
     if(users.length > 2){
         console.log( users[0] + "," + users[1] + " and 3 more are online ");
     }
+
+
+    // Exercise 1 : The World Translator
+
+    user = prompt("which language they speak");
+
+    // question 2
+    user.toLowerCase();
+
+    // question 3 
+    switch(user){
+        case "French":
+            console.log("Bonjour !");
+            break;
+
+            case "English":
+                console.log("say Hello");
+                break;
+
+                case "Hebrew":
+                    console.log("Shalom");
+                    break;
+
+                    default:
+                        console.log("01110011 01101111 01110010 01110010 01111001");
+                        break;
+    }
+
+    // Exercise 2 : The Grade Assigner
+
+    // question 1 
+    userGrade = prompt("what is you grade ");
+
+    // question 2 
+    if(userGrade > 90){
+        console.log("A");
+    }
+
+    // question 3
+    if(userGrade >= 80 && userGrade <= 90){
+        console.log("B");
+    }
+
+    // question 4
+    if(userGrade >= 70 && userGrade <= 80){
+        console.log("C");
+    }
+
+    // question 5 
+    if (userGrade < 70) {
+        console.log("D");
+    }
+
+    // Exercise 3 : Verbing
+
+    // question 1
+    let user = prompt("send us the verb");
+    let result1;
+
+    // Question 2
+    var last3 = user.slice(-3);
+    if (user.length > 3 && last3 !== "ing") {
+        result1 = user.concat("ing")
+    }
+
+    console.log(result1);
+
+
+       // question 2
+       let user2 = prompt("send us the verb");
+       let result2;
+   
+       // Question 2
+       var last3 = user.slice(-3);
+       if (user.length > 3 && last3 !== "ly") {
+           // result2 = user.replace(last3, "ly");
+           result2 = user.concat("ly");
+       }
+   
+       console.log(result2);
+
+       // question 3 
+       if (user2.length < 3) {
+           console.log(user2);
+       }
