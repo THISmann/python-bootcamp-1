e6d145cd9858d258485206cf578a47a00dc63c27
THISmann <fuhetienne0@gmail.com>
2022-05-04 19:59:21 +0100

    python exo

---
+++ week6/day2/exo/exo2.py
@@ -0,0 +1,14 @@
+number = int(input("intrer votre nombre"))
+table = list(range(1 , 12))
+
+for tab in table:
+    print(f"{tab} * {number} = {tab * number}")
+    
+    
+
+num = 1
+while num < 11:
+    print(num)
+    num += 1 
+    
+
