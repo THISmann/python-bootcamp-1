8d188cdff4c97991d8c7ef82d21fa07cec68f4f3
THISmann <fuhetienne0@gmail.com>
2022-05-05 05:39:22 +0100

    save exo

--- week6/day2/exo/exo2.py
+++ week6/day2/exo/exo2.py
@@ -12,3 +12,14 @@ while num < 11:
     num += 1 
     
 
+userString = input('votre string svp')
+if len(userString) > 10:
+    print(" String too long")
+else:
+    print(" string is shoot")
+    
+        
+    
+
+for i in list(range(1 , len(stg))):
+     print(stg[0:i + 1])
