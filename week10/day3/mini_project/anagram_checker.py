e36f37906765d81628a273c99238c580851f9e52
THISmann <fuhetienne0@gmail.com>
2022-05-25 19:15:30 +0100

    pythom

---
+++ week10/day3/mini_project/anagram_checker.py
@@ -0,0 +1,8 @@
+class AnagramChecker():
+    def __init__(self, file):
+        self.file = file
+        f =  open(f"{file}", "r")
+    
+    def is_valid_word(self, word):
+        if word in 
+         
