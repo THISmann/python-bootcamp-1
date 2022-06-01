5421018ee044bd36ca223e743ee81feda6e432fe
THISmann <fuhetienne0@gmail.com>
2022-06-01 19:52:13 +0100

    flask start

---
+++ week10/revision/fake.py
@@ -0,0 +1,8 @@
+import datetime
+
+today_date = datetime.date.today()
+actual_datetime = datetime.datetime.now()
+in_15_hours = actual_datetime + datetime.timdelta(hours=15, minutes=10)
+
+print(f"Today is the {today_date.day}/{today_date.month}")
+print(f"In 15 hours and 10 minutes it will be the {in_15_hours.day}/{in_15_hours.month}")
