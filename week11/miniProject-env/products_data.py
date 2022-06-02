e77690e42cebe5dbdaa12c3c0a1131d8f6660166
THISmann <fuhetienne0@gmail.com>
2022-06-02 19:22:35 +0100

    product

--- week11/miniProject-env/products_data.py
+++ week11/miniProject-env/products_data.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 import json
 
 
 def retrieve_all_products():
-    with open('product.json', 'r') as f:
-        all_products = json.load(f)
-    return render_template('product.html', data=all_products)
+    all_product = 'product.json'
+    with open(all_product, 'r') as file:
+        datas = json.load(all_product)
+        print(datas)
 
+    retrieve_all_products()
 
-def retrieve_requested_product(id):
+# def retrieve_requested_product():
+#     data = retrieve_all_products()
+#     print(data)
+#     for i in data.keys():
+#         print(i)
+
+#     datas = retrieve_requested_product()
+#     print(datas)
