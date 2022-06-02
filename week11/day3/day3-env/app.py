8b6b9313e41d2f1c465d912d8a19279ec0a31c71
THISmann <fuhetienne0@gmail.com>
2022-06-02 16:45:11 +0100

    day 3

---
+++ week11/day3/day3-env/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5500)
+
+
+@app.route('/<username>')
+def home():
+    return render_template('homepage.html', name=username)
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', data=['one', 'two'])
