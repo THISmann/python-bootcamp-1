d399aa54a2bbd8067d71078eaf8b9d807d56ae2b
THISmann <fuhetienne0@gmail.com>
2022-06-21 05:51:21 +0100

    Form

--- week13/todo-list/app/models.py
+++ week13/todo-list/app/models.py
@@ -8,29 +8,32 @@ from app import app, db
 # db = flask_sqlalchemy.SQLAlchemy(app)
 migrate = flask_migrate.Migrate(app, db)
 
+
 class MyModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     details = db.Column(db.String(64), index=True)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    details = db.Column(db.String(64), index=True) 
-    completed = db.Column(db.Boolean, index=True, default=False , server_default="false")
-    
+    details = db.Column(db.String(64), index=True)
+    completed = db.Column(db.Boolean, index=True,
+                          default=False, server_default="false")
+
     def __repr__(self):
         return '<Todo %r>' % self.id
-    
-    def save_task_to_db(self):
-        db.session.add(self)
+
+    def save_task_to_db(seft, data):
+        db.session.add(data)
         db.session.commit()
-        
+
     def get_tasks(self):
-         return self.query.all()
+        return self.query.all()
 
     # def set_task_as_complete(self):
     #     pass
-    
+
     # def __init__(self, email, password):
     #     self.email = email
     #     self.set_password(password)
@@ -55,5 +58,5 @@ class Todo(db.Model):
 
     # def get_id(self):
     #     return str(self.email)
-    
+
     db.create_all()
--- week13/todo-list/app/routes.py
+++ week13/todo-list/app/routes.py
@@ -1,29 +1,38 @@
 from flask import Flask
-import flask , flask_wtf , wtforms
+import flask
+import flask_wtf
+import wtforms
 from app import app, db
-from app.forms import Addtodo 
+from app.forms import Addtodo
 
-from app.models import MyModel , Todo
+from app.models import MyModel, Todo
+
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     users = Addtodo()
     if users.validate_on_submit():
         details = users.details.data
-        Todo.save_task_to_db(Todo)
-        return flask.render_template('index.html', form = users)
+        data = Todo(details=users.details.data)
+        Todo.save_task_to_db(data)
+        return flask.render_template('index.html', form=users)
     else:
         Todo.get_tasks(Todo)
-        return flask.render_template('index.html', form = users)
-        
+        return flask.render_template('index.html', form=users)
+
+
 @app.route("/complete/<int:todo_id>")
-def set_task_as_complete(): 
+def set_task_as_complete():
     todo = Todo.query(todo_id)
     todo.completed = True
     db.session.commit()
+    
+@app.route("/all")
+def get_all():
+    tasks = Todo.get_tasks()
 
 # todos = MyModel.query.all()
 # db.session.add(Todo(id=1 , details="TP 22"))
 # db.session.commit()
 
-#print(Todo.query.all())
+# print(Todo.query.all())
