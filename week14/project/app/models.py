96d1fa1806d55824842038183fdeed2621b4418d
THISmann <fuhetienne0@gmail.com>
2022-06-25 11:01:50 +0100

    model

--- week14/project/app/models.py
+++ week14/project/app/models.py
@@ -10,9 +10,19 @@ migrate = flask_migrate.Migrate(app, db)
 
 
 class MyModel(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     details = db.Column(db.String(64), index=True)
+    
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.relationship('User', backhref = 'task' , lazy= "dynamic")
+    executant = db.Column(db.String(64), index=True)
+    date_end = db.Column(db.String(64), index=True)
+    data_start = db.Column(db.String(64), index=True)
+    author = db.Column(db.String(64), index=True)
+    description = db.Column(db.String(64), index=True)
+    status = db.Column(db.String(64), index=True)
+    author = db.Column(db.String(64), index=True) #relationship
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,13 +31,11 @@ class User(db.Model):
     password = db.Column(db.String(64), index=True)
     status_session = db.Column(db.String(64), index=True)
     role = db.Column(db.String(64), index=True)
+    task = db.Column(db.String(64), db.ForeygnKey('task.id')
+    author = db.Column(db.String(64), db.ForeygnKey('task.id')
     
-class Task(db.Model):
+
+    
+class Rode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
-    executant = db.Column(db.String(64), index=True)
-    date_end = db.Column(db.String(64), index=True)
-    data_start = db.Column(db.String(64), index=True)
-    author = db.Column(db.String(64), index=True)
-    description = db.Column(db.String(64), index=True)
-    status = db.Column(db.String(64), index=True)
