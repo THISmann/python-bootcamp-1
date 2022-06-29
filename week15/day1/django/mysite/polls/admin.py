c311b85f893ce8a33d1ef14dc8dbcbb1a70e6cbf
THISmann <fuhetienne0@gmail.com>
2022-06-29 08:58:11 +0100

    django

--- week15/day1/django/mysite/polls/admin.py
+++ week15/day1/django/mysite/polls/admin.py
@@ -1,3 +1,6 @@
+# Register your models here.
 from django.contrib import admin
+from .models import Person  # import the Person model
 
 # Register your models here.
+admin.site.register(Person)
--- week15/day1/django/mysite/polls/models.py
+++ week15/day1/django/mysite/polls/models.py
@@ -1,3 +1,12 @@
+# import the models package. This line is already existing as soon as we use 'startapp'
 from django.db import models
 
-# Create your models here.
+# Must inherit from Django Model class
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    birth_date = models.DateField()
+    has_pet = models.BooleanField(default=True)
+    #number_pet = models.IntegerField(max_value=10)
