180594265d474c68687ee4eb144b79bdcb537ccd
THISmann <fuhetienne0@gmail.com>
2022-06-30 05:43:31 +0100

    admin

--- week15/day1/django/mysite/polls/admin.py
+++ week15/day1/django/mysite/polls/admin.py
@@ -1,6 +1,8 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Person  # import the Person model
+from .models import *  # import the Person model
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Task)
+admin.site.register(Post)
--- week15/day1/django/mysite/polls/models.py
+++ week15/day1/django/mysite/polls/models.py
@@ -1,15 +1,36 @@
 # import the models package. This line is already existing as soon as we use 'startapp'
 from django.db import models
+from datetime import datetime, date
 
 # Must inherit from Django Model class
 
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=30)
     birth_date = models.DateField()
     has_pet = models.BooleanField(default=True)
-    #number_pet = models.IntegerField(max_value=10)
+    #number_pet = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def person_age(self):
+        current_date = date.today()
+        current_age = current_date.year-self.birth_date.year
+        return f'{current_age}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.CharField(max_length=50)
+    released_date = models.DateField(default=datetime.now())
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
+    # If you delete a person, his posts will be also deleted
+
+    def __str__(self):
+        return f'{self.title}'
 
 
 class Task(models.Model):
