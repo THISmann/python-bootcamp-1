cd35ed12985dac4dca3a2ace5e560a38c27a9fce
THISmann <fuhetienne0@gmail.com>
2022-07-12 18:13:42 +0100

    app

--- e_class/backend/auth/models.py
+++ e_class/backend/auth/models.py
@@ -6,16 +6,16 @@ from datetime import datetime, date
 
 class Student(models.Model):
     username = models.CharField(max_length=200)
-    classe = models.ForeignKey(Classe, on_delete=models.PROTECT)
+    #classe = models.ForeignKey(Classe, on_delete=models.PROTECT)
     email = models.EmailField()
     password = models.CharField(max_length=200)
     phone_numero = models.IntegerField()
-    etablissement = models.ForeignKey(Etablissement)
+    #etablissement = models.ForeignKey(Etablissement)
+
 
 class Teacher(models.Model):
     username = models.CharField(max_length=200)
     classe = models.ForeignKey(Classe, on_delete=models.PROTECT)
     email = models.EmailField()
     password = models.CharField(max_length=200)
-    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
-    
+    #topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
