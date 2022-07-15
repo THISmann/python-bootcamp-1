e8225c295f3a08036873ce81c775155b64454e73
THISmann <fuhetienne0@gmail.com>
2022-07-15 22:34:40 +0200

    model

--- e_class/backend/studentrecord/models.py
+++ e_class/backend/studentrecord/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime , date 
 
+class Discipline(models.Model):
+    #student_name = models.ForeignKey(to=Student , on_delete=)
+    school_years = models.DateField()
+    
 # Create your models here.
--- e_class/backend/studymanagement/models.py
+++ e_class/backend/studymanagement/models.py
@@ -10,4 +10,56 @@ class Course(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=200)
     phone_numero = models.IntegerField()
+    content = models.CharField(max_length=255)
+    #instructor = models.ForeignKey(to=Instructor)
+    create_at = models.DateField(max_length=55)
+    number_of_hour = models.IntegerField()
+
     #etablissement = models.ForeignKey(Etablissement)
+
+    def __str__(self):
+        return self.name
+
+
+class homework(models.Model):
+    #topic = models.ManyToManyField(to=topic )
+    #course = models.ManyToManyField()
+    #classe = models.ManyToManyField()
+    content = models.CharField(max_length=255)
+    create_at = models.DateField()
+    date_end = models.DateField()
+    #author = models.ForeignKey(to=Teacher)
+
+    def __str__(self):
+        return self.topic
+
+    pass
+
+
+class topic(models.Model):
+    name = models.CharField(max_length=255)
+    # class = models.ManyToManyField()
+    #instructor = models.ManyToManyField()
+
+    def __str__(self):
+        return self.name
+
+    pass
+
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=255)
+    #head_teacher = models.ForeignKey(to, on_delete)
+    # define the head teacher who will manage the class
+
+    def __str__(self):
+        return self.name
+
+    pass
