71df0b07b7cd1f2f4ff1eba5303c31dd5fe6e661
THISmann <fuhetienne0@gmail.com>
2022-08-07 15:32:47 +0200

    update

---
+++ e_class/frontend/src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar(props) {
+    return (
+        <>
+            <div className="navbar" >
+                <h1> navbar</h1>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ e_class/frontend/src/containers/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+import Navbar from "../../components/navbar/navbar";
+
+function Home(props) {
+    return (
+        <>
+            <h1> home </h1>
+            <Navbar />
+        </>
+    );
+}
+
+export default Home;
